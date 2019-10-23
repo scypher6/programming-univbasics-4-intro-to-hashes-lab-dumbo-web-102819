c32e4890e07d90ebae08dd204267bbfe88456cb4
Joseph Pyram <joseph.pyram@gmail.com>
2019-10-23 04:32:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,11 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-    if ()
+    if hash[key]
+       return hash[key]
+    end  
+    
+    return nil
 end
 
 def update_counting_hash(hash, key)
